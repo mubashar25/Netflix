cf836cb8d33b4eef4b4487f6cbfec23d7867e890
mubashar25 <mubasharpervaiz826@gmail.com>
2026-03-16 09:01:28 +0500

    Updated project files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 
-dotenv.config();
 connectDB();
 
 const app = express();
